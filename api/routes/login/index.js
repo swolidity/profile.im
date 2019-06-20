bb9a92c1dcb165c952eb387aeea6b44d45e14e9e
Andy Kay <andykay.me@gmail.com>
2019-06-20 16:23:03 -0400

    facebook login WIP

--- api/routes/login/index.js
+++ api/routes/login/index.js
@@ -1,19 +1,28 @@
 const express = require("express");
+const passport = require("passport");
+const FacebookStrategy = require("passport-facebook").Strategy;
 const helmet = require("helmet");
 const app = express();
-const json = require("body-parser").json;
 const mongo = require("mongo");
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: "http://localhost:3000/api/login"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+      return cb(null, profile);
+    }
+  )
+);
 
 // add some security-related headers to the response
 app.use(helmet());
-app.use(json());
+app.use(require("body-parser").urlencoded({ extended: true }));
+app.use(passport.initialize());
 
-app.get("*", async (req, res) => {
-  const db = await mongo();
-  const usersCollection = await db.collection("users");
-  const users = await usersCollection.find().toArray();
-
-  res.send(users);
-});
+app.get("*", passport.authenticate("facebook", { session: false }));
 
 module.exports = app;
