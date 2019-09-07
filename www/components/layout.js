1745f52d158ef2da4f22b0b46b56d0f8763c456c
Andy Kay <andykay.me@gmail.com>
2019-09-07 05:46:39 -0400

    main padding

--- www/components/layout.js
+++ www/components/layout.js
@@ -53,6 +53,7 @@ const Layout = ({ children }) => {
           .main {
             max-width: 800px;
             margin: 0 auto;
+            padding: 0 32px;
           }
           .profile-pic {
             height: 45px;
